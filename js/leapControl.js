8dc5b478c00b6654a5d08cfc90fffccdc043f24d
Katrin Schäfers <online@kschaefers.de>
2015-12-10 20:18:39 +0100

    * added debug mode and other config * added functionality for volume up/down, channel up/down and mute

--- js/leapControl.js
+++ js/leapControl.js
@@ -1,7 +1,11 @@
 var leapController;
 
 $(document).ready(function () {
-    var lastCommand;
+    var lastCommand,
+        lastTime5FingersExtended,
+        lastTimeFist,
+        volumeMode = false,
+        palmPositionForVolumeMode;
 
     leapController = new Leap.Controller();
 
@@ -16,28 +20,68 @@ $(document).ready(function () {
     leapController.connect();
 
     Leap.loop({enableGestures: true}, function (frame) {
-        var newDate = new Date();
+        var newDate = new Date(),
+            newDateMinusHalfSecond = new Date(newDate).setSeconds(newDate.getSeconds() - 0.5),
+            numberOfHands = frame.hands.length,
+            numberOfFingersExtended = 0;
 
-        if (!lastCommand || lastCommand < newDate.setSeconds(newDate.getSeconds() - 0.5)) {
+        if (numberOfHands > 0) {
+            frame.pointables.forEach(function (pointable) {
+                if (pointable.extended) {
+                    numberOfFingersExtended += 1;
+                }
+            });
+        }
+
+        if (numberOfHands === 1 && numberOfFingersExtended === 5 && volumeMode) {
+            var delta,
+                currentPalmPosition = frame.hands[0].palmPosition[1];
+            if (!palmPositionForVolumeMode) {
+                palmPositionForVolumeMode = currentPalmPosition;
+            } else {
+                delta = currentPalmPosition - palmPositionForVolumeMode;
+                if (delta > config.sensibilityInVolumeMode) {
+                    palmPositionForVolumeMode = currentPalmPosition;
+                    volumeUp();
+                } else if (delta < config.sensibilityInVolumeMode * -1) {
+                    palmPositionForVolumeMode = currentPalmPosition;
+                    volumeDown();
+                }
+            }
+        } else {
+            volumeMode = false;
+        }
+
+        if (!lastCommand || lastCommand < newDateMinusHalfSecond) {
+
+            // one hand detected and all 5 fingers extended
+            if (numberOfHands === 1) {
+                if (numberOfFingersExtended === 5) {
+                    var lastTimeFistHalfSecond = new Date(lastTimeFist).setSeconds(newDate.getSeconds() - 0.5);
+                    if (lastTime5FingersExtended && lastTime5FingersExtended > lastTimeFistHalfSecond && lastTimeFist > newDateMinusHalfSecond) {
+                        mute();
+                        lastCommand = new Date();
+                    }
+                    lastTime5FingersExtended = new Date();
+                } else if (numberOfFingersExtended === 0) {
+                    lastTimeFist = new Date();
+                }
+            }
+
+            //volume Mode
+            volumeMode = !!(numberOfHands === 1 && numberOfFingersExtended === 5);
 
             if (frame.gestures.length > 0 && frame.gestures[0].type === 'swipe') {
-                if (frame.gestures[0].direction[0] < 0) {
-                    // rechts nach links
-
-                    $.get('http://localhost:8080/?samsung=ChUp', function (data, status) {
-                    }, 'html');
-                } else {
-                    // links nach rechts
-                    $.get('http://localhost:8080/?samsung=ChDown', function (data, status) {
-                    }, 'html');
+                //make sure that the direction was made on the x axis
+                if (Math.abs(frame.gestures[0].direction[0]) > Math.abs(frame.gestures[0].direction[1])
+                    && Math.abs(frame.gestures[0].direction[0]) > Math.abs(frame.gestures[0].direction[2])) {
+                    if (frame.gestures[0].direction[0] < 0) { // rechts nach links
+                        channelUp();
+                    } else { // links nach rechts
+                        channelDown();
+                    }
+                    lastCommand = new Date();
                 }
-                lastCommand = new Date();
-            } else if (frame.gestures.length > 0 && frame.gestures[0].type === 'screenTap') {
-
-                lastCommand = new Date();
-            } else if (frame.gestures.length > 0 && frame.gestures[0].type === 'keyTap') {
-
-                lastCommand = new Date();
             }
         }
     });
---
+++ js/tvHandlingFunctions.js
@@ -0,0 +1,50 @@
+function channelUp() {
+    if (config.debug) {
+        console.log('channel Up');
+    } else {
+        $.get('http://localhost:8080/?' + config.tv + '=ChUp', function (data) { /* what to do with the data returned */
+        })
+    }
+}
+
+function channelDown() {
+    if (config.debug) {
+        console.log('channel Down');
+    } else {
+        $.get('http://localhost:8080/?' + config.tv + '=ChDown', function (data) { /* what to do with the data returned */
+        })
+    }
+}
+
+function volumeUp() {
+    if (config.debug) {
+        console.log('volume Up');
+    } else {
+        $.get('http://localhost:8080/?' + config.tv + '=VolUp', function (data) { /* what to do with the data returned */
+        })
+    }
+}
+
+function volumeDown() {
+    if (config.debug) {
+        console.log('volume Down');
+    } else {
+        $.get('http://localhost:8080/?' + config.tv + '=VolDown', function (data) { /* what to do with the data returned */
+        })
+    }
+}
+
+function mute() {
+    if (config.debug) {
+        console.log('mute');
+    }
+
+}
+
+function power() {
+
+}
+
+function enterNumber() {
+
+}
