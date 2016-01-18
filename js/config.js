f1c57d1fcf79cd7dace18e5e1ae895794c7d031d
Katrin Schäfers <online@kschaefers.de>
2016-01-18 16:11:54 +0100

    deleted unused files

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 var config = {
     tv: 'samsung', //has to match the parameter in the url from switchBoard,
     sensibilityInVolumeMode: 40, // the higher the number the lower the sensibility
-    controlTV: true     // will send the calls to the TV if true
+    controlTV: false     // will send the calls to the TV if true
 };
